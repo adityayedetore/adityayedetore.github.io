f5fcecd70a47e46ac3a540852f1579cd5948d3e3
adityayedetore <50057764+adityayedetore@users.noreply.github.com>
2026-04-19 20:28:46 +0000

    Deploying to gh-pages from @ adityayedetore/adityayedetore.github.io@fde1b99af1f82836bcdbaa56f9e334f7ec144ba7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-spotlight-talk-amp-amp-poster-at-nenlp-2024-on-semantic-bootstrapping-in-neural-networks",
+          title: 'Spotlight talk &amp;amp;amp; poster at NENLP 2024 on semantic bootstrapping in neural networks....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
